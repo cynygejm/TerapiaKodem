0cdb404130971b79595102cf642378d242e12554
cynygejm <asiaglina@o2.pl>
2017-11-29 22:26:56 +0100

    szkic js

--- terapia.js
+++ terapia.js
@@ -1,2 +1,10 @@
-const cont = document.querySelector(.container);
-const app = document.querySelector(#app);
+const container = document.querySelector(.container);
+const btnList = document.querySelector(#ToDoList);
+const btnPass=document.querySelector(#PasswordGenerator);
+const btnKosci=document.querySelector(#Kosci);
+btnList.addEventListener('click', function showApp {
+})
+btnKosci.addEventListener('click', function showApp {
+})
+btnPass.addEventListener('click', function showApp {
+})
